1a20f8b300ea7f4cf74ba80e725f269987ea4c97
Bairam Batirov <hamraev.bairm@gmail.com>
2019-09-19 11:39:45 +0200

    Initial commit

---
+++ app/src/main/java/com/bairam/snake2/Audio.java
@@ -0,0 +1,8 @@
+package com.bairam.snake2;
+
+public interface Audio {
+
+public Music newMusic(String fileName);
+
+public Sound newSound(String filename);
+}
---
+++ app/src/main/java/com/bairam/snake2/Sound.java
@@ -0,0 +1,8 @@
+package com.bairam.snake2;
+
+public interface Sound {
+
+    public void play(float volume);
+
+    public void dispose();
+}
