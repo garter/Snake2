3c10d949a5f6945132326719a0a87b2b636bf1c8
Bairam Batirov <hamraev.bairm@gmail.com>
2019-09-20 18:47:21 +0200

    add 2 interfaces

---
+++ app/src/main/java/com/bairam/snake2/Pixmap.java
@@ -0,0 +1,9 @@
+package com.bairam.snake2;
+
+public interface Pixmap {
+
+    public int getWidth();
+    public int getHeight();
+    public Graphics.PixmapFormat getFormat();
+    public void dispose();
+}
